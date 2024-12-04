8080e55e48000d4e8754884723ca5bdcde4485b0
Hemant Shrestha <sthahemant1st@gmail.com>
2024-12-04 09:01:16 +0545

    Initial Commit

---
+++ SchemeDemo/SchemeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchemeDemoApp.swift
+//  SchemeDemo
+//
+//  Created by Hemant Shrestha on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SchemeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
